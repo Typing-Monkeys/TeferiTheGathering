3e20b06880d9eae5860c2d8f6183bab5987727fe
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-02-07 10:08:22 +0100

    Aggiunta Defender
    - Implementata defender
    - Aggiunta carta 2892 al mazzo
    
    Co-Authored-By: juandres117

--- mtggameinterface/script/clientL.js
+++ mtggameinterface/script/clientL.js
@@ -217,6 +217,9 @@ window.onload = function() {
     deck1.push(2891)
     deck1.push(2891)
 
+    deck1.push(2892)
+    deck1.push(2892)
+    deck1.push(2892)
 
 // - DECK 2 DECLARATION ----------------------------------------------------------------------------- 
     var deck2= Array(); // White/Blu
